37b7cbe5f5eaaede38cad728f8ac4c861a5ed307
Kyle Thomas <kylejwthomas@gmail.com>
2024-05-22 17:50:13 +0900

    Setting properties

--- CustomButton.cs
+++ CustomButton.cs
@@ -9,5 +9,25 @@ namespace Calculator
         private int borderSize = 0;
         private int borderRadius = 50;
         private Color borderColor = Color.DodgerBlue;
+
+        //Properties
+        public int BorderSize
+        {
+            get => borderSize;
+            set { borderSize = value; Invalidate(); } 
+        }
+        public int BorderRadius
+        {
+            get => borderRadius;
+            set { borderRadius = value; Invalidate(); }
+        }
+        public Color BackgroundColor
+        {
+            get => BackColor; set { BackColor = value; }
+        }
+        public Color TextColor
+        {
+            get => ForeColor; set { ForeColor = value; }
+        }
     }
 }
