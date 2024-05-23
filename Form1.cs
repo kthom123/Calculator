68d50503a0cb866ca8f01b5a89cb04c287a8c94c
Kyle Thomas <kylejwthomas@gmail.com>
2024-05-23 15:09:15 +0900

    Edit text display code

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,13 @@ namespace Calculator
             if (TxtDisplay1.Text == "0" || enterValue) TxtDisplay1.Text = string.Empty;
 
             enterValue = false;
+            CustomButton button = new CustomButton();
+            if (button.Text == ".")
+            {
+                if (TxtDisplay1.Text.Contains("."))
+                    TxtDisplay1.Text = TxtDisplay1.Text + button.Text;
+            }
+            else TxtDisplay1.Text = TxtDisplay1.Text + button.Text;
         }
     }
 }
